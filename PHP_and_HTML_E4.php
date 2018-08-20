5645cc52d7d5ee23ea61f5f4225c1809a2fa2bd2
Mike <MikeAMarchlewski@users.noreply.github.com>
2018-08-19 19:17:22 -0500

    End Lesson

--- PHP_and_HTML_E4.php
+++ PHP_and_HTML_E4.php
@@ -16,13 +16,19 @@
            <h1> 
                 <?php 
 
+                    //THe following allows "/?name = 'name input'" in URL to change $name
+
+                    //name in $_GET['name'] cane be changed to anything else and done the same in URL
+
                    //$name = $_GET['name'];
 
                     //echo 'Hello, ' . $name; 
 
                     //Can also do the same as the following:
 
-                    echo "Hello, " . $_GET['name'];
+                    echo "Hello, " . htmlspecialchars($_GET['name']);
+
+                    //htmlspecialchars is a built in PHP function to keep user from making input a URL
 
                 ?> 
                 
