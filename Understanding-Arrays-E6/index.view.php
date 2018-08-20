668c1929689184549cbbde8cf6fcc6ac93314ca0
Mike <MikeAMarchlewski@users.noreply.github.com>
2018-08-19 19:40:29 -0500

    Start of Lesson

---
+++ Understanding-Arrays-E6/index.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+
+header {
+    background: #e3e3e3;
+    padding: 2em;
+    text-align: center;
+}
+</style>
+    
+</head>
+<body>
+
+    <header>
+
+        <h1>
+            
+        </h1>
+    </header>
+    
+</body>
+</html>
